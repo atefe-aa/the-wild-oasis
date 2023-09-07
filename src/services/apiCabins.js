cddba3a79f9e5f44218a5662887636d412e813db
atefe-aa <aa.asgariyan12@gmail.com>
2023-09-07 21:31:00 +0330

    implement create cabin form functionality

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -13,7 +13,6 @@ export async function getCabins() {
   return data;
 }
 
-
 export async function deleteCabin(id) {
   const res = await fetch(`http://127.0.0.1:8000/api/cabins/${id}`, {
     method: "DELETE",
@@ -24,6 +23,26 @@ export async function deleteCabin(id) {
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be deleted.");
+  }
+
+  return success;
+}
+
+export async function createCabin(newCabin) {
+  const res = await fetch(`http://127.0.0.1:8000/api/cabins`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newCabin),
+  });
+  const { success, error } = await res.json();
+
+  if (error) {
+    const { name, regular_price, max_capacity } = error;
+    console.error(error);
+    if (name) throw new Error(name);
+    if (max_capacity) throw new Error(max_capacity);
+    if (regular_price) throw new Error(regular_price);
+    throw new Error(error);
   }
 
   return success;
