1e170ae1008351b2f469e03b9c179ee9841ebbd7
atefe-aa <aa.asgariyan12@gmail.com>
2023-09-16 18:35:13 +0330

    new version of the modal , useing compound component pattern

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,24 +1,36 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateEditCabinForm from "./CreateEditCabinForm";
 import Modal from "../../ui/Modal";
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <span>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
-
-      {isOpenModal && (
-        <Modal onClose={()=>setIsOpenModal(false)}>
-          <CreateEditCabinForm  onCloseModal={()=>setIsOpenModal(false)}/>
-        </Modal>
-      )}
-    </span>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateEditCabinForm />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <span>
+//       <Button onClick={() => setIsOpenModal((show) => !show)}>
+//         Add new cabin
+//       </Button>
+
+//       {isOpenModal && (
+//         <Modal onClose={()=>setIsOpenModal(false)}>
+//           <CreateEditCabinForm  onCloseModal={()=>setIsOpenModal(false)}/>
+//         </Modal>
+//       )}
+//     </span>
+//   );
+// }
 
 export default AddCabin;
