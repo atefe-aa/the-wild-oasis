8703908f957ccb3380ce1c3e0f5a9834708ed16c
atefe-aa <aa.asgariyan12@gmail.com>
2023-09-23 08:10:59 +0330

    implement perefetching to the pagination to improve user experience

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,16 +1,33 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
   const [searchParams] = useSearchParams();
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
-
-  const { isLoading, data:{bookings, pageData} = {}, error } = useQuery({
+  const queryClient = useQueryClient();
+  const {
+    isLoading,
+    data: { bookings, pageData } = {},
+    error,
+  } = useQuery({
     queryKey: ["bookings", page],
     queryFn: () => getBookings(page),
   });
 
+  // console.log(pageData);
+  //PRE-FETCHING
+  if (page < pageData?.last_page)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", page + 1],
+      queryFn: () => getBookings(page + 1),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", page - 1],
+      queryFn: () => getBookings(page - 1),
+    });
 
   return { isLoading, error, bookings, pageData };
 }
