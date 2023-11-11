e06bdcba3c909f72880a31ab60e69d03683927fb
Atefe <aa.asgariyan12@gmail.com>
2023-11-11 09:05:31 +0330

    change the favicon and update pakages

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const PAGE_SIZE = 10;
-export const API_BASE_URL = "http://localhost:8000/api";
-export const STORAGE_URL = "http://127.0.0.1:8000/storage/";
+// export const API_BASE_URL = "https://the-wild-oasis.iran.liara.run/api";
+export const API_BASE_URL = "http://127.0.0.1:8000/api";
+// export const STORAGE_URL = "http://127.0.0.1:8000/storage/";
